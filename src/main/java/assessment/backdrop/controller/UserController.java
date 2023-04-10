c4aafb8a7fe922401a5aced6e63e56075e2dea56
apimpnamedodogwu <edenelenwoke@gmail.com>
2023-04-10 12:09:35 +0100

    fetch

--- src/main/java/assessment/backdrop/controller/UserController.java
+++ src/main/java/assessment/backdrop/controller/UserController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -21,7 +18,7 @@ public class UserController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     private final GraphQLService graphQLService;
 
-    @GetMapping("")
+    @PostMapping("")
     public ResponseEntity<ExecutionResult> verifyUser(@RequestBody String query) {
         try {
             ExecutionResult executionResult = graphQLService.getGraphQL().execute(query);
--- src/main/java/assessment/backdrop/service/user/FetchVerifiedUser.java
+++ src/main/java/assessment/backdrop/service/user/FetchVerifiedUser.java
@@ -1,5 +1,6 @@
 package assessment.backdrop.service.user;
 
+import assessment.backdrop.data.dto.UserRequest;
 import assessment.backdrop.data.model.User;
 import assessment.backdrop.repository.UserRepository;
 import graphql.schema.DataFetcher;
@@ -9,13 +10,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class FetchVerifiedUser implements DataFetcher<User> {
-    private final UserRepository userRepository;
+public class FetchVerifiedUser implements DataFetcher<String> {
+    private final UserServiceImpl userService;
 
     @Override
-    public User get(DataFetchingEnvironment environment) throws Exception {
+    public String get(DataFetchingEnvironment environment) throws Exception {
         String acctNumber = environment.getArgument("accountNumber");
         String bankCode = environment.getArgument("bankCode");
-        return userRepository.findByAccountNumberAndBankCode(acctNumber, bankCode);
+        return userService.verifyUser(UserRequest.builder()
+                .accountNumber(acctNumber)
+                .bankCode(bankCode)
+                .build());
     }
 }
